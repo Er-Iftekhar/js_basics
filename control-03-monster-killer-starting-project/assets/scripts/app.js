9cff41d1d66e192297ce294f9ebce30099129b22
Iftekharuddin Syed <syedy636@gmail.com>
2024-10-05 21:59:09 +0200

    Add constant strings in the code for reusability.

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -4,6 +4,9 @@ const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 const enteredValue = prompt('Maximum life for you and the monster');
 
+const MODE_ATTACK = 'ATTACK';
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+
 let chosenMaxLife = parseInt(enteredValue);
 if(isNaN(enteredValue) || chosenMaxLife <= 0){
     chosenMaxLife = 100;
@@ -49,9 +52,9 @@ function endRound(){
 
 function attackMonster(mode){
     let maxDamage
-    if (mode === 'ATTACK'){
+    if (mode === MODE_ATTACK){
         maxDamage = ATTACK_VALUE;
-    } else if (mode === 'STRONG_ATTACK') {
+    } else if (mode === MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE;
     }
 
@@ -61,11 +64,11 @@ function attackMonster(mode){
 }
 
 function attackHandler(){
-    attackMonster('ATTACK');
+    attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler(){
-    attackMonster('STRONG_ATTACK');
+    attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler(){
